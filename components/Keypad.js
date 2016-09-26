be71d23c516be147ad0d3fda8db64d0ea0d5bc36
Francis Rocco <roccofrancis@gmail.com>
2016-09-26 14:08:18 -0400

    learn event system react

---
+++ components/Keypad.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Keypad extends React.Component {
+  render() {
+    return(
+      <div>
+        <input type="password" onKeyUp={console.log('Entering password...')}/>
+      <div>
+    )
+  }
+}
